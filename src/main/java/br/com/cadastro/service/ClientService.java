34c4f34c3f7c2281c6bc62db281583de072d9f37
Thiago Jose <thiago.wilton2704@gmail.com>
2024-10-04 20:04:21 -0300

    Implementa o ClientService com operações de criação, listagem, validação de credenciais e exclusão de clientes
    - Adicionado método `createClient` para criar e salvar clientes, associando contas e endereço ao cliente.
    - Método `listClient` adicionado para listar um cliente por ID.
    - Método `validateCredentials` implementado para verificar email e senha do cliente.
    - Método `deleteClient` para exclusão de cliente com confirmação do usuário através de uma interação no terminal.

---
+++ src/main/java/br/com/cadastro/service/ClientService.java
@@ -0,0 +1,60 @@
+package br.com.cadastro.service;
+
+import br.com.cadastro.model.account.AccountModel;
+import br.com.cadastro.model.address.AddressModel;
+import br.com.cadastro.model.client.ClientModel;
+import br.com.cadastro.repository.ClientRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+@Service
+public class ClientService {
+    @Autowired
+    private ClientRepository clientRepository;
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Transactional
+    public void createClient(ClientModel clientModel, List<AccountModel> accountModel, AddressModel addressModel) {
+        clientModel.setAddressModel(addressModel);
+
+        List<AccountModel> updatedAccountModels = new ArrayList<>();
+
+        for (AccountModel model : accountModel) {
+            model.setClientModel(clientModel);
+            updatedAccountModels.add(model);
+        }
+
+        clientModel.setAccountModel(updatedAccountModels);
+        clientRepository.save(clientModel);
+    }
+
+    public void listClient(Long id) {
+        Optional<ClientModel> client = clientRepository.findById(id);
+        System.out.println(client);
+    }
+
+    public boolean validateCredentials(String email, String password) {
+        Optional<ClientModel> client = clientRepository.findByEmailAndPassword(email, password);
+        return client.isPresent();
+    }
+
+    public void deleteClient(Long id) {
+        while (true) {
+            System.out.println("você tem certeza que deseja deletar seu usuário (s/n) ?");
+            String resp = scanner.nextLine().trim().toUpperCase();
+            if (resp.equals("N")) {
+                break;
+            } else if (resp.equals("S")) {
+                clientRepository.deleteById(id);
+                break;
+            }
+            System.out.println("Resposta inválida!");
+        }
+    }
+}
