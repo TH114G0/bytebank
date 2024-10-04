d604a3cc9adb0770894569d93ec945d63984d9b4
Thiago Jose <thiago.wilton2704@gmail.com>
2024-10-04 15:07:49 -0300

    Criando query para validação de login e senha.

--- src/main/java/br/com/cadastro/repository/ClientRepository.java
+++ src/main/java/br/com/cadastro/repository/ClientRepository.java
@@ -2,5 +2,12 @@ package br.com.cadastro.repository;
 
 import br.com.cadastro.model.client.ClientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ClientRepository extends JpaRepository<ClientModel, Long> {}
+import java.util.Optional;
+
+public interface ClientRepository extends JpaRepository<ClientModel, Long> {
+    @Query("SELECT c FROM ClientModel c WHERE c.email = :email AND c.password = :password")
+    Optional<ClientModel> findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+}
