5ac83e0ae9debe54a1e7ec783aed43f9467d5d0a
Thiago Jose <thiago.wilton2704@gmail.com>
2024-10-04 19:59:36 -0300

    Implementa método para listar endereço por ID com tratamento de exceção
    - Adicionado método `listAddress(Long id)` no serviço para buscar e listar um endereço pelo seu ID.
    - Utiliza `Optional` e lança `EntityNotFoundException` com uma mensagem personalizada caso o endereço não seja encontrado.

--- src/main/java/br/com/cadastro/service/AddressService.java
+++ src/main/java/br/com/cadastro/service/AddressService.java
@@ -1,13 +1,23 @@
 package br.com.cadastro.service;
 
 
+import br.com.cadastro.model.address.AddressModel;
 import br.com.cadastro.repository.AddressRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AddressService {
     @Autowired
     private AddressRepository addressRepository;
 
+    public void listAddress(Long id) {
+        Optional<AddressModel> optionalAddress  = addressRepository.findById(id);
+        AddressModel addressModel = optionalAddress.orElseThrow(() -> new EntityNotFoundException("Endereço não encontrado para o ID: " + id));
+        System.out.println(addressModel);
+    }
 }
