1fb747f8028d84fd1bb4ac7b827edaa1ab0bba2d
Thiago Jose <thiago.wilton2704@gmail.com>
2024-10-04 15:04:24 -0300

    Criando Services

---
+++ src/main/java/br/com/cadastro/service/AccountService.java
@@ -0,0 +1,12 @@
+package br.com.cadastro.service;
+
+import br.com.cadastro.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+}
